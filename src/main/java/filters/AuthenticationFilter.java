02db79eb159d1224561168b31c7de77e4e04ff3c
nguyn_huynh <huynhphuocnguyen2412@gmail.com>
2024-12-19 03:45:30 +0700

    1

--- src/main/java/filters/AuthenticationFilter.java
+++ src/main/java/filters/AuthenticationFilter.java
@@ -8,8 +8,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import models.bean.Account;
 import models.bean.Company;
+import models.bean.User;
 import models.bo.CompanyBO;
-import models.dao.CompanyDAO;
+import models.bo.UserBO;
 
 import java.io.IOException;
 
@@ -36,10 +37,22 @@ public class AuthenticationFilter implements Filter {
             throw new UnauthorizedException("Bạn phải đăng nhập!");
         }
 
-        if(account.getRoleName().equals("Company")){
-           CompanyBO companyBO = new CompanyBO();
-           Company company = companyBO.getCompanyByAccountId(account.getId());
-           session.setAttribute("companyId", company.getId());
+        if (account.getRoleName().equals("Company")) {
+            CompanyBO companyBO = new CompanyBO();
+
+            Company company = companyBO.getCompanyByAccountId(account.getId());
+            if (company == null) {
+                throw new UnauthorizedException("Tài khoản không tồni");
+            }
+            session.setAttribute("companyId", company.getId());
+        }
+
+        if (account.getRoleName().equals("User")) {
+            User user = new UserBO().getUserByAccountId(account.getId());
+            if (user == null) {
+                throw new UnauthorizedException("Tài khoản không tồn tại");
+            }
+            session.setAttribute("userId", user.getId());
         }
 
         chain.doFilter(request, response);
